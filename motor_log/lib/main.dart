a0cb118d181b77ca58eea1b11b6bbba96b68a76e
SFIRegret19 <fspirin8@gmail.com>
2026-04-12 15:01:14 +0700

    Добавлен Weather API и secure keys с dotenv

--- motor_log/lib/main.dart
+++ motor_log/lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'core/theme/app_theme.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'presentation/screens/garage_screen.dart';
 
 void main() async {
   // ЭТА СТРОКА ОБЯЗАТЕЛЬНА:
   // Она инициализирует связь между кодом Dart и нативной частью (Android/iOS)
   WidgetsFlutterBinding.ensureInitialized();
+
+  // ЗАГРУЖАЕМ КЛЮЧИ ИЗ ФАЙЛА .env
+  await dotenv.load(fileName: ".env");
 
   runApp(const MotorLogApp());
 }
